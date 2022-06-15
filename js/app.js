bb783024c6fcb39e18d47263fa06a7e88a9b85e9
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-06-15 21:35:23 +0200

    Adding validation to the fields, they get red if you click on them and you dont write anything.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,36 @@
 //Variables
 const btnEnviar = document.querySelector('#enviar');
 
+//Variables para campos.
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje')
 
 //Functions
 eventListeners();
 function eventListeners(){
     document.addEventListener('DOMContentLoaded', iniciarApp);
 
+    //Campo del formulario
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur',validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
+
 }
 
 function iniciarApp(){
    btnEnviar.disabled  = true;
    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+//Valida el  formulario
+
+function validarFormulario(e){
+
+    if (e.target.value.length > 0) {
+        console.log('Si hay algo')
+    } else {
+        e.target.classList.add('border', 'border-red-500');
+        console.log('No hay nada');
+    }
 }
